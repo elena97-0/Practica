b7f84ba621df3fd14a52b16e40dece38e2f172ec
elena97-0 <lenadefacto@hotmail.com>
2025-04-03 17:11:08 +0200

    Update FileConverterApp.java

--- src/main/java/app/FileConverterApp.java
+++ src/main/java/app/FileConverterApp.java
@@ -1,11 +1,14 @@
 package app;
 import menu.Menu;
 
-import java.awt.*;
-
+/**
+ * Clase principal que inicia la aplicación de conversión de archivos.
+ */
 public class FileConverterApp {
     public static void main(String[] args) {
+        // Crea una instancia de la clase Menu
         Menu menu = new Menu();
+        // Método que inicia el menú interactivo
         menu.start();
     }
 }
