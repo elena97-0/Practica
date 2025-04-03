7ab531d3df245243b730dbcd2dcf411e82297fc5
elena97-0 <lenadefacto@hotmail.com>
2025-04-03 17:14:26 +0200

    Update CSVHandler.java

--- src/main/java/handlers/CSVHandler.java
+++ src/main/java/handlers/CSVHandler.java
@@ -7,7 +7,12 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.*;
 
+///Clase que maneja la lectura y escritura de archivos CSV.
 public class CSVHandler {
+
+    /// @param filePath Ruta del archivo CSV a leer.
+    /// @return Lista de mapas con los datos del archivo.
+    
     public static List<Map<String, String>> readCSV(Path filePath) {
         List<Map<String, String>> dataList = new ArrayList<>();
         try (BufferedReader br = Files.newBufferedReader(filePath)) {
@@ -27,6 +32,10 @@ public class CSVHandler {
         return dataList;
     }
 
+    /// Escribe una lista de mapas en un archivo CSV.
+    /// @param filePath Ruta donde se guardará el archivo CSV
+    /// @param data Lista de mapas que contienen los datos a escribir.
+    
     public static void writeCSV(Path filePath, List<Map<String, String>> data) {
         try (BufferedWriter bw = Files.newBufferedWriter(filePath)) {
             if (data.isEmpty()) return;
