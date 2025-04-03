f3c2fb074aea8ffed2d4e2dac1d56c109f4f799a
elena97-0 <lenadefacto@hotmail.com>
2025-04-03 17:16:10 +0200

    Update FileHandler.java

--- src/main/java/handlers/FileHandler.java
+++ src/main/java/handlers/FileHandler.java
@@ -4,7 +4,14 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
+/// Clase que maneja la lectura y escritura de archivos en distintos formatos.
+/// Soporta CSV y JSON.
 public class FileHandler {
+
+    /// Lee un archivo de datos y lo convierte en una lista de mapas.
+    /// Identifica el formato del archivo (CSV o JSON) según su extensión.
+    /// @param filePath Ruta del archivo a leer.
+    /// @return Lista de mapas con los datos del archivo o null si el formato no es soportado
     public static List<Map<String, String>> readFileData(Path filePath) {
         String extension = getFileExtension(filePath.toString());
         return switch (extension) {
@@ -17,6 +24,10 @@ public class FileHandler {
         };
     }
 
+    /// Escribe datos en un archivo en el formato especificado (CSV o JSON)
+    /// @param filePath Ruta del archivo donde se guardarán los datos.
+    /// @param data Lista de mapas con los datos a escribir
+    /// @param format Formato del archivo de salida (csv o json).
     public static void writeFile(Path filePath, List<Map<String, String>> data, String format) {
         switch (format) {
             case "csv" -> CSVHandler.writeCSV(filePath, data);
@@ -24,7 +35,10 @@ public class FileHandler {
             default -> System.out.println("Formato no válido.");
         }
     }
-
+    
+    /// Obtiene la extensión de un archivo a partir de su nombre.
+    /// @param filename Nombre o ruta del archivo.
+    /// @return Extensión del archivo (sin el punto) o una cadena vacía si no tiene extensión
     private static String getFileExtension(String filename) {
         int dotIndex = filename.lastIndexOf('.');
         return (dotIndex == -1) ? "" : filename.substring(dotIndex + 1);
